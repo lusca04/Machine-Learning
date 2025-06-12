a068f27e25c9835bc8719fbb42eaff17e1802c54
Lucas Rodrigues <162514161+lusca04@users.noreply.github.com>
2025-06-12 20:00:03 -0300

    Updating Main

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
-from Configurations.MqttConfig import PublishMessage
+from Configurations.MttConfig import PublishMessage
 
 # model = YOLO("yolov8x.pt")#modelo nao treinado  
 # OBS: yolov8X.pt = melhor qualidade || yolov8n.pt = menor qualidade, melhor pra testes
 model = YOLO("./MachineLearningTraining/runs/detect/train2/weights/best.pt") #modelo treinado com yolov8n.pt
 
-for result in model.predict(source = "0", show = True, stream = True):
+for result in model.predict(source = "0", show = True, stream = True):  
+    #Source 0 -> webcam, strem = true -> codigo conseguir validar em tempo real
     caixa_errada = False
     for box in result.boxes:
         cls = int(box.cls[0])
